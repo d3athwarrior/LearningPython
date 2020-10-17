f6fbdf3c53bdc66a5897f741501ddc2c35a88095
d3athwarrior <anish114@gmail.com>
2020-10-17 21:54:02 +0530

    Play around with indentations

---
+++ BlocksAndIndentations.py
@@ -0,0 +1,10 @@
+x = 10
+y = 15
+
+if (x > y):
+  print(x)
+else:
+    print(y)
+  #print('hello') # This line when uncommented, will give error as it is not indented as per the other blocks before this
+
+# The whole idea is that when in a block, the entire block should be at the same level.
