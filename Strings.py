ac7ffdc0296f3d69d7684a6fad13232478885c80
d3athwarrior <anish114@gmail.com>
2021-01-16 23:26:50 +0530

    Split and join strings

--- Strings.py
+++ Strings.py
@@ -9,3 +9,13 @@ y = 'Hi {name}, you are a {profession}'.format(**person)
 print('The value of x is {}'.format(x))
 print(f'The type of x is {type(x)}') # This 'f' notation is called f string notation where in the format function is called implicitly
 print(y)
+
+s = 'Hello this    is a      very long string.'
+"""
+    This by default will split the string based on the spaces even if there are too many spaces,
+    they all will be folded into single space and then split basis of that.
+"""
+print(s.split())
+
+# join function takes in a list of strings and joins them on the string on which join was called
+print(':'.join(s.split()))
